fb891435369a17880df4f3991cffa67e3f3cd2f4
Sreejith <sreejimunthikodu@yahoo.com>
2020-07-07 20:25:07 -0700

    changed line graph to percapita, bar to horizontal

--- app.py
+++ app.py
@@ -16,8 +16,16 @@ app = dash.Dash(
 server = app.server
 
 # Clean the data and generate plots
-def main(confirmed, deaths, recovered):
+def main(confirmed, deaths, recovered, population):
     output = {}
+
+    # Get population data
+    pop = pd.read_csv(population, skiprows=2, header=1)
+    pop = pop[['Country Name', '2019']]
+    pop = pop.replace('United States', 'US')
+    pop = pop.replace('Russian Federation', 'Russia')
+    pop.columns = ['country', 'population']
+
     # declare columns to retain after melting the data
     required_columns = ['Province/State', 'Country/Region', 'Lat', 'Long'] 
 
@@ -38,6 +46,9 @@ def main(confirmed, deaths, recovered):
     # Group by to country granularity
     df = df.groupby(['country', 'date']).sum().reset_index()
 
+    # Join population data
+    df = df.merge(pop, 'left', left_on='country', right_on='country')
+
     # Gete list of all countries
     list_of_countries = df['country']
     output['list_of_countries'] = list_of_countries
@@ -48,7 +59,7 @@ def main(confirmed, deaths, recovered):
 
     # Plot latest summary
     df_summary = dfl.sum()
-    df_summary = pd.DataFrame(df_summary).T.drop(["country", "Lat", "Long"], axis=1).T
+    df_summary = pd.DataFrame(df_summary).T.drop(["country", "Lat", "Long", "population"], axis=1).T
     df_summary.columns = ["total"]
     df_summary["total"] = df_summary["total"].astype("int64")
     df_summary = df_summary.reset_index().sort_values(by="total", ascending=False)
@@ -97,9 +108,10 @@ path_data = "data/csse_covid_19_time_series"
 confirmed = os.path.join(path_data, "time_series_covid19_confirmed_global.csv")
 deaths = os.path.join(path_data, "time_series_covid19_deaths_global.csv")
 recovered = os.path.join(path_data, "time_series_covid19_recovered_global.csv")
+population = os.path.join("data/world_population.csv")
 
 # Load and clean the data
-output = main(confirmed, deaths, recovered)
+output = main(confirmed, deaths, recovered, population)
 latest_date = output['latest_date'] 
 start_date = output['start_date'] 
 dates = [start_date + timedelta(days=x) for x in range((latest_date-start_date).days + 1)]
@@ -256,30 +268,32 @@ def update_figure(selected_country, time_range):
     start = dates_dict[time_range[0]]
     end = dates_dict[time_range[1]]    
     dfn = df.query("country == @selected_country and date >= @start and date <= @end")
+    # Confirmed cases per 10000 population
+    dfn["Confirmed"] = dfn["Confirmed"]/dfn["population"]*10000
     # Plot timeline
     fig_tl = px.line(dfn, x="date", y="Confirmed", color='country')
-    # for trace in fig_tl.data:
-    #     trace.name = trace.name.split('=')[1]
+    for trace in fig_tl.data:
+        trace.name = trace.name.split('=')[1]
 
     fig_tl.update_layout(xaxis_showgrid=False, yaxis_showgrid=False, yaxis_title_text="",
-                                xaxis_title_text="", showlegend=True, title=f"Timeline of Total Confirmed Cases Between {start} and {end}",
+                                xaxis_title_text="", showlegend=True, title=f"Total Confirmed Cases per 10,000 Population Between {start} and {end}",
                                 plot_bgcolor="#323130", paper_bgcolor="#323130", font=dict(color="white"),
                                 margin= {'t': 50, 'b': 10, 'l': 10, 'r': 0})
 
     # Plot summary of selected countries
     dfn = dfl.query("country == @selected_country")
     dfn = dfn.melt(id_vars=['country'], value_vars=['Confirmed', "Recovered", 'Deaths'])
-    fig_sum = px.bar(dfn, x="variable", y="value", facet_col='country', color='country')
-    # for trace in fig_sum.data:
-    #     trace.name = trace.name.split('=')[1]
+    fig_sum = px.bar(dfn, y="variable", x="value", facet_row='country', color='country', orientation='h')
+    for trace in fig_sum.data:
+        trace.name = trace.name.split('=')[1]
 
     fig_sum.for_each_annotation(lambda a: a.update(text=""))
-    fig_sum.for_each_xaxis(lambda a: a.update(title=""))    
+    fig_sum.for_each_xaxis(lambda a: a.update(showgrid=False, title=""))    
     fig_sum.for_each_yaxis(lambda a: a.update(showgrid=False, title=''))
-    fig_sum.update_xaxes(tickangle=90)
+    fig_sum.update_xaxes(tickangle=0)
 
     fig_sum.update_layout(bargroupgap=0, bargap=0.1, plot_bgcolor="#323130", paper_bgcolor="#323130", font=dict(color="white"),
-                          title=f"Total Cases as on {latest_date}", showlegend=True)
+                          title=f"Total Cases as on {latest_date}", showlegend=False)
 
     # Plot change in last 24 hours for the selected countries
     dfn_24 = dfl_change_summary.query("country == @selected_country")
@@ -287,17 +301,17 @@ def update_figure(selected_country, time_range):
     dfn_24 = dfn_24.rename(columns={'Change in 24 hours, Confirmed' : "Confirmed", "Change in 24 hours, Recovered" : "Recovered", 'Change in 24 hours, Deaths' : "Deaths"})
     dfn_24 = dfn_24.melt(id_vars=['country'], value_vars=["Confirmed", "Recovered", "Deaths"])
 
-    fig_24 = px.bar(dfn_24, x="variable", y="value", facet_col='country', color='country')
-    # for trace in fig_24.data:
-    #     trace.name = trace.name.split('=')[1]
+    fig_24 = px.bar(dfn_24, y="variable", x="value", facet_row='country', color='country', orientation='h')
+    for trace in fig_24.data:
+        trace.name = trace.name.split('=')[1]
     fig_24.for_each_annotation(lambda a: a.update(text=""))
-    fig_24.for_each_xaxis(lambda a: a.update(title=""))    
+    fig_24.for_each_xaxis(lambda a: a.update(showgrid=False, title=""))    
     fig_24.for_each_yaxis(lambda a: a.update(showgrid=False, title=''))
     fig_24.update_layout(bargroupgap=0, bargap=0.1, plot_bgcolor="#323130", paper_bgcolor="#323130", font=dict(color="white"),
                          title=f"New cases in the Last 24 Hours ({latest_date})")
-    fig_24.update_xaxes(tickangle=90)
+    fig_24.update_xaxes(tickangle=0)
 
     return [fig_tl, fig_sum, fig_24]
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True, port=8001)
